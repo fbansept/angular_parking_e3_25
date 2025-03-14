b58910388d18f8920e21a82374147dde7f0004b1
fbansept <bansept.franck@gmail.com>
2025-03-14 12:40:12 +0100

    connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {AccueilComponent} from './accueil/accueil.component';
+import {ConnexionComponent} from './connexion/connexion.component';
+import {Page404Component} from './page404/page404.component';
+import {ReservationPlaceComponent} from './reservation-place/reservation-place.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "accueil", component: AccueilComponent},
+  {path : "connexion", component: ConnexionComponent},
+  {path : "reservation-place/:id", component: ReservationPlaceComponent},
+  {path : "", redirectTo: "accueil", pathMatch:"full"},
+  {path : "**", component:Page404Component}
+];
---
+++ src/app/model/place.d.ts
@@ -0,0 +1,4 @@
+type Place = {
+  id: number,
+  numero: number
+}
